936232b54d11728e5ac29801cb35bb2585753ff9
its-popplewell <39974659+its-popplewell@users.noreply.github.com>
2026-05-23 13:37:02 -0400

    Go RL Test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tandjgamejam
+You need to run `go get github.com/gen2brain/raylib-go/raylib` to download the Raylib dependency
--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-// "fmt"
-// "io"
-// "log"
-// "net/http"
-// "os"
-// "path/filepath"
-// "sort"
-// "time"
-//
-// rl "github.com/gen2brain/raylib-go/raylib"
+	// "fmt"
+	// "io"
+	// "log"
+	// "net/http"
+	// "os"
+	// "path/filepath"
+	// "sort"
+	// "time"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
-	println("Hello World!")
+	rl.InitWindow(800, 600, "WPC GIF Player – click‑drag to pan")
+	defer rl.CloseWindow()
+	rl.SetWindowState(rl.FlagWindowResizable)
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+
+		rl.DrawRectangle(0, 0, 30, 30, rl.Red)
+
+		rl.EndDrawing()
+	}
 }
