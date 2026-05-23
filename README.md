c989d2cbf3974a84811cbbd0479216f050296b57
Tushar Aggarwal <tusharagg12@gmail.com>
2026-05-23 14:38:56 -0400

    basic movement and readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # tandjgamejam
 You need to run `go get github.com/gen2brain/raylib-go/raylib` to download the Raylib dependency
+
+
+BONK & FIGHT
+SHOP SHTUFF
+COMBINE AND META CODE
+
+an auto battling game where dinosaurs with helmets run into eachother and fight. 
+
+you can buy helmets and modifiers
+and then you can build different builds of helmets and stuff 
+
+player movements player health
+
+fighting:
+- running
+- bonking
+- helmet
+  - hp/dmg/# hits
+  - helmet has modifier slots
+  - modifiers can take up more than one slot
+  -
--- main.go
+++ main.go
@@ -13,17 +13,40 @@ import (
 )
 
 func main() {
+	// INITIALIZATION
 	rl.InitWindow(800, 600, "WPC GIF Player – click‑drag to pan")
 	defer rl.CloseWindow()
+
+	// FLAGS
 	rl.SetWindowState(rl.FlagWindowResizable)
 
+	// OBV
 	rl.SetTargetFPS(60)
 
+	// var x = 0
+	// var x int = 0
+	var x int32 = 0
+	var y int32 = 0
+
+	// RENDERING LOOP
 	for !rl.WindowShouldClose() {
+		if rl.IsKeyDown(rl.KeyD) {
+			x += 1
+		}
+		if rl.IsKeyDown(rl.KeyA) {
+			x -= 1
+		}
+		if rl.IsKeyDown(rl.KeyW) {
+			y -= 1
+		}
+		if rl.IsKeyDown(rl.KeyS) {
+			y += 1
+		}
+
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
 
-		rl.DrawRectangle(0, 0, 30, 30, rl.Red)
+		rl.DrawRectangle(x, y, 30, 30, rl.Red)
 
 		rl.EndDrawing()
 	}
