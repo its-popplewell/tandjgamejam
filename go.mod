fa851518de8a56a1e4f96205a8d5efd4774c6c75
its-popplewell <39974659+its-popplewell@users.noreply.github.com>
2026-05-23 13:32:19 -0400

    Added Go stuff

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tandjgamejam
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+// "fmt"
+// "io"
+// "log"
+// "net/http"
+// "os"
+// "path/filepath"
+// "sort"
+// "time"
+//
+// rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func main() {
+	println("Hello World!")
+}
